f769945520d57b72113e2e239986422326751db1
liyoyan <545230170@qq.com>
2024-01-02 20:11:43 +0800

    feat:添加串口初始化文件

---
+++ code/infrastructure/bsp/robomaster_hero/bsp_usart.h
@@ -0,0 +1,33 @@
+/**
+*********************************************************************************************************
+* Copyright (C) 2023-2026 by liyoyan - All Rights Reserved.                                              *
+*                                                                                                       *
+* This file is part of the project.                                                                     *
+*                                                                                                       *
+* This file can not be copied and/or distributed without the express permission of the project owner.   *
+*********************************************************************************************************/
+
+/**
+*********************************************************************************************************
+* @file   : bsp_usart.h
+* @author : liyoyan
+* @date   : 2 JAN 2024
+* @brief  :
+*
+*********************************************************************************************************
+*/
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include "stm32f4xx.h"
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
